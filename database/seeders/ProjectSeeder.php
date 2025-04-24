bc8b5f99560b86a2350bb95315e067cc96cac3ff
giuseppecasaburi <enzobiondi0000@libero.it>
2025-04-24 23:19:14 +0200

    Aggiungi il modello Project, la migrazione per la tabella projects e il seeder per generare dati fittizi

---
+++ database/seeders/ProjectSeeder.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Project;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Faker\Generator as Faker;
+
+class ProjectSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(Faker $faker): void
+    {
+        for ($i = 0; $i < 5; $i++) {
+            $newProject = new Project();
+
+            $newProject->name = $faker->name;
+            $newProject->client = $faker->name;
+            $newProject->project_start = $faker->dateTimeThisYear();
+            $newProject->project_end = $faker->dateTimeThisYear("+1 months");
+            $newProject->description = $faker->paragraph();
+
+            $newProject->save();
+        }
+    }
+}
